34e3e3a99ee7d99e00c870c50dfa67ac787d9eb4
lusteiger <leonardoxy7@gmail.com>
2020-10-23 10:46:06 +0800

    [xinyu lu] implement oder list page

--- front-end/src/compoment/orders/orders.js
+++ front-end/src/compoment/orders/orders.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Button, Table } from "antd";
+import "antd/dist/antd.css";
 class orders extends Component {
-          render() {
-                    return (
-                              <div>
-                                       orders 
-                              </div>
-                    );
-          }
+  state = {
+    column: [
+      {
+        title: "#",
+        dataIndex: "id",
+        render: (text) => <a>{text}</a>,
+      },
+      {
+        title: "名称",
+        dataIndex: "name",
+        align: "right",
+      },
+      {
+        title: "单价",
+        className: "column-money",
+        dataIndex: "unit",
+      },
+      {
+        title: "数量",
+        dataIndex: "number",
+      },
+    ],
+
+    data: [
+      {
+        id: "1",
+        name: "可乐",
+        price: "￥3.00",
+        number: "1",
+      }
+      
+    ],
+  };
+
+  render() {
+    return (
+      <Table
+        columns={this.state.column}
+        dataSource={this.state.data}
+        bordered
+        title={() => "订单编号" }
+      />
+    );
+  }
 }
 
 export default orders;
