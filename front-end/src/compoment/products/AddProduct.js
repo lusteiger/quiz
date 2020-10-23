3375895d899ad3b7f708376b9df51256cef119bf
lusteiger <leonardoxy7@gmail.com>
2020-10-23 09:46:44 +0800

    [xinyu lu] implement the add product page

--- front-end/src/compoment/products/AddProduct.js
+++ front-end/src/compoment/products/AddProduct.js
@@ -1,13 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddProduct extends Component {
-          render() {
-                    return (
-                              <div>
-                                        AddProduct
-                              </div>
-                    );
-          }
+  state = {
+    name: "",
+    price: "",
+    unit: "",
+    image: "",
+  };
+
+  handleFieldChange = (field, event) => {
+    this.setState({
+      [field]: event.target.value,
+    });
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(this.state));
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <p>添加货品</p>
+        <div>
+          <p>名称：</p>
+          <input
+            type="text"
+            placeholder="名称"
+            value={this.state.name}
+            id="name"
+            onChange={(e) =>this.handleFieldChange("name",e)}
+          />
+
+          <p>价格：</p>
+          <input
+            type="text"
+            placeholder="price"
+            value={this.state.price}
+            id="price"
+            onChange={(e) =>this.handleFieldChange("price",e)}
+          />
+          <p>单位：</p>
+          <input
+            type="text"
+            placeholder="单位"
+            value={this.state.unit}
+            id="unit"
+            onChange={(e) =>this.handleFieldChange("unit",e)}
+          />
+          <p>图片：</p>
+          <input
+            type="text"
+            placeholder="图片"
+            value={this.state.image}
+            id="image"
+            onChange={(e) =>this.handleFieldChange("image",e)}
+          />
+          <br/>
+           <input type="submit" ></input>
+        </div>
+       
+      </form>
+    );
+  }
 }
 
 export default AddProduct;
