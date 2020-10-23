738ec09ff0f541703345ee4d96b4c384758dd63b
lusteiger <leonardoxy7@gmail.com>
2020-10-23 11:02:46 +0800

    [xinyu lu] optimize navigator

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Navigators from './compoment/Navigators/Navigator';
+import "antd/dist/antd.css";
 function App() {
   return (
     <div className="App">
--- front-end/src/compoment/Navigators/Navigator.js
+++ front-end/src/compoment/Navigators/Navigator.js
@@ -10,20 +10,28 @@ import "./Navigators.css";
 import Orders from "../orders/orders";
 import ProductsList from "../products/productsList";
 import AddProduct from "../products/AddProduct";
+import { Menu } from "antd";
 class Navigator extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Router>
-          <Link to="/">商城</Link>
-          <Link to="/Orders">订单</Link>
-          <Link to="/AddProduct">添加商品</Link>
-          <Switch>
-            <Route exact path="/" component={ProductsList} />
-            <Route exact path="/AddProduct" component={AddProduct} />
-            <Route exact path="/Orders" component={Orders} />
-          </Switch>
-        </Router>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">
+            <Link to="/">商城</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/Orders">订单</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/AddProduct">添加商品</Link>
+          </Menu.Item>
+        </Menu>
+
+        <Switch>
+          <Route exact path="/" component={ProductsList} />
+          <Route exact path="/AddProduct" component={AddProduct} />
+          <Route exact path="/Orders" component={Orders} />
+        </Switch>
       </BrowserRouter>
     );
   }
